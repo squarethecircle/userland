1c25644355ecddc8a70c2dd7dc6adfa36cf613a2
Sachith Gullapalli <sachith.gullapalli@yale.edu>
2015-06-16 05:44:23 +0000

    use pin defines

--- host_applications/linux/apps/raspicam/RaspiStill.c
+++ host_applications/linux/apps/raspicam/RaspiStill.c
@@ -88,6 +88,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define MMAL_CAMERA_VIDEO_PORT 1
 #define MMAL_CAMERA_CAPTURE_PORT 2
 
+#define SHUTDOWN_PIN 3
+#define LED_PIN 2
 
 // Stills format information
 // 0 implies variable
@@ -1528,7 +1530,7 @@ static int wait_for_next_frame(RASPISTILL_STATE *state, int *frame)
 
    int64_t current_time =  vcos_getmicrosecs64()/1000;
 
-   if (shutdown_flag && digitalRead(0))
+   if (shutdown_flag && digitalRead(SHUTDOWN_PIN))
          system("shutdown -P now");
       
    if (complete_time == -1)
@@ -1854,7 +1856,7 @@ void* gps_update(void* sharedData_void)
 
 void shutdown(void)
 {
-   digitalWrite (2, HIGH);
+   digitalWrite (LED_PIN, HIGH);
 	shutdown_flag = true;
 }
 
@@ -1862,8 +1864,8 @@ void* flashLED(void* arg)
 {
    while(true)
    {
-      digitalWrite (2, HIGH); delay (1000);
-      digitalWrite (2,  LOW); delay (1000);
+      digitalWrite (LED_PIN, HIGH); delay (1000);
+      digitalWrite (LED_PIN,  LOW); delay (1000);
    }
 }
 
@@ -1875,12 +1877,12 @@ int main(int argc, const char **argv)
 {
    shutdown_flag = false;
    wiringPiSetup();
-   pinMode(3,INPUT);
-   pullUpDnControl(3,PUD_UP);
-   pinMode(2,OUTPUT);
+   pinMode(SHUTDOWN_PIN,INPUT);
+   pullUpDnControl(SHUTDOWN_PIN,PUD_UP);
+   pinMode(LED_PIN,OUTPUT);
    pthread_t startCamera;
    pthread_create(&startCamera, NULL, flashLED, NULL);
-   wiringPiISR(3,INT_EDGE_RISING,shutdown);
+   wiringPiISR(SHUTDOWN_PIN,INT_EDGE_RISING,shutdown);
    piHiPri(99);
 
    int serial_ret = serialOpen("/dev/ttyAMA0",19200);
